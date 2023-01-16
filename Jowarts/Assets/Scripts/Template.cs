81b8b189d8d2a55365864a12643aacc42314f53d
dnavas <dnavas@ggtech.es>
2023-01-16 12:36:00 +0100

    Peliada inicial

---
+++ Jowarts/Assets/Scripts/Template.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Template : MonoBehaviour
+{
+    #region Private Variables
+    
+    #endregion
+
+    #region Unity LifeCycle
+
+    
+
+    #endregion
+
+    #region Utility Methods
+
+    
+
+    #endregion
+}
